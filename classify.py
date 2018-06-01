891ea90223bbb0a98eef399e3502db8a3a256eca
Evan <2523815778@qq.com>
2018-06-01 21:33:51 +0800

    single label classifcation

--- classify.py
+++ classify.py
@@ -1,194 +1,177 @@
-from sklearn import svm, base, decomposition, preprocessing
+# -*- coding: utf-8 -*-
+#-------------------------------------------------------------------------------------#
+#  Name:           classify.py                                                        #
+#  Description:    single-label classification based on linear SVM                    #
+#  Author:         fyl                                                                #
+#-------------------------------------------------------------------------------------#
+from sklearn import metrics, preprocessing, svm, decomposition
+from sklearn.pipeline import FeatureUnion, Pipeline
+from sklearn.model_selection import GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.feature_extraction.text import TfidfVectorizer
-from scipy.sparse import vstack, issparse
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 import numpy as np
-import json
-import random
-import argparse
 import pickle
-from pprint import pprint
-from crawl import CATEGORIES
-
-from pprint import pprint
-
-class RankJudge:
-
-    def __init__(self, binary_classifier, q, verbose=0):
-        self.base_clf = binary_classifier
-        self.q = q
-        self.verbose = verbose
-
-    def fit(self, X, Y):
-        is_sparse = issparse(X)
-
-        self.clfs = [[base.clone(self.base_clf)
-                      for _ in range(self.q - 1 - i)] for i in range(self.q)]
-
-        for i in range(self.q):
-            for j in range(i + 1, self.q):
-                X_tmp, Y_tmp = [], []
-                for xi, yi in zip(X, Y):
-                    if yi[i] != yi[j]:
-                        X_tmp.append(xi)
-                        Y_tmp.append(int(yi[i] > yi[j]))
-                    else:
-                        X_tmp += [xi, xi]
-                        Y_tmp += [0, 1]
-                if is_sparse:
-                    X_tmp = vstack(X_tmp, format='csr')
-                if self.verbose >= 1:
-                    print('.', end='', flush=True)
-                if 0 in Y_tmp and 1 in Y_tmp:
-                    self.clfs[i][j - i - 1].fit(X_tmp, Y_tmp)
-                else:
-                    self.clfs[i][j - i - 1] = Y_tmp[0]
-
-    def predict(self, X):
-        xlen = X.shape[0] if issparse(X) else len(X)
-
-        res = [[0] * self.q for _ in range(xlen)]
-        for i in range(self.q):
-            for j in range(i + 1, self.q):
-                if self.clfs[i][j - i - 1] in {0, 1}:
-                    pred = [self.clfs[i][j - i - 1]] * xlen
-                else:
-                    pred = self.clfs[i][j - i - 1].predict(X)
-                for index in range(xlen):
-                    if pred[index] == 1:
-                        res[index][i] += 1
-                    else:
-                        res[index][j] += 1
-        return res
+import argparse
+import time
 
 
-class MultiRankClassifier:
+class MyCountVectorizer(CountVectorizer):
+    """
+    Extract the last column of the input matrix (which is app description) and
+    simply pass it to CountVectorizer.
 
-    def __init__(self, binary_classifier,  max_iters=20, kfold=3, verbose=0):
-        self.clf = binary_classifier
-        self.max_iters = max_iters
-        self.kfold = kfold
-        self.verbose = verbose
+    Parameters
+    ----------
+    (identical to the parameters of CountVectorizer)
+    """
 
-    def _split_k(self, X, Y, k):
-        is_sparse = issparse(X)
+    def __init__(self, input='content', encoding='utf-8', decode_error='strict',
+                 strip_accents=None, lowercase=True, preprocessor=None, tokenizer=None,
+                 stop_words=None, token_pattern=r'(?u)\b\w\w +\b', ngram_range=(1, 1),
+                 analyzer='word', max_df=1.0, min_df=1, max_features=None, vocabulary=None,
+                 binary=False, dtype=np.int64):
 
-        if not is_sparse:
-            X = np.array(X)
+        return super().__init__(input=input, encoding=encoding, decode_error=decode_error,
+                                strip_accents=strip_accents, lowercase=lowercase, preprocessor=preprocessor,
+                                tokenizer=tokenizer, stop_words=stop_words, token_pattern=token_pattern,
+                                ngram_range=ngram_range, analyzer=analyzer, max_df=max_df, min_df=min_df,
+                                max_features=max_features, vocabulary=vocabulary, binary=binary, dtype=dtype)
 
-        if X.shape[0] < k:
-            raise ValueError(
-                'not enough samples to be split into %d partitions' % k)
-        elif X.shape[0] != len(Y):
-            raise ValueError(
-                'number of samples do not match, %d in X and %d in Y' % (X.shape[0], len(Y)))
-        div, mod = divmod(X.shape[0], k)
-        n_each = [div + 1] * (mod) + [div] * (k - mod)
-        ans = 0
+    def fit(self, x, *args, **kwargs):
+        return super().fit(x[:, -1].tolist(), *args, **kwargs)
 
-        X_, Y_, X_c = [], [], []
-        for i in range(k):
-            X_.append(X[ans:ans + n_each[i]])
-            Y_.append(Y[ans:ans + n_each[i]])
-            if is_sparse:
-                X_c.append(
-                    vstack((X[:ans], X[ans + n_each[i]:]), format='csr'))
-            else:
-                X_c.append(np.concatenate(
-                    (X[:ans], X[ans + n_each[i]:]), axis=0))
-            ans += n_each[i]
+    def fit_transform(self, x, *args, **kwargs):
+        return super().fit_transform(x[:, -1].tolist(), *args, **kwargs)
 
-        return X_, Y_, X_c
-
-    def fit(self, X, Y):
-        if isinstance(Y, np.ndarray):
-            Y = Y.tolist()
-
-        xlen = X.shape[0] if issparse(X) else len(X)
-
-        is_sparse = issparse(X)
-
-        tmp = list(zip(range(xlen), X, Y))
-        random.shuffle(tmp)
-        old_index, X, Y = zip(*tmp)
-        X, Y = list(X), list(Y)
-        
-        if is_sparse:
-            X = vstack(X, format='csr')
-        
-
-        self.q = len(Y[0])
-        X, Y, Xc = self._split_k(X, Y, self.kfold)
-        rankj = RankJudge(self.clf, self.q, self.verbose)
-        Y_next = [None] * self.kfold
-
-        for iteration in range(self.max_iters):
-            if self.verbose >= 1:
-                print('[%d]' % iteration, end='', flush=True)
-            for k in range(self.kfold):
-                Yc = sum([Y[i] for i in range(self.kfold) if i != k], [])
-                rankj.fit(Xc[k], Yc)
-                Y_next[k] = rankj.predict(X[k])
-            Y = Y_next
-
-            Y_tmp = sum(Y, [])
-            Y_old = [None] * len(Y_tmp)
-            for i in range(len(Y_tmp)):
-                Y_old[old_index[i]] = Y_tmp[i]
-            with open('tmp/%d_%d.json' % (xlen, iteration), 'w') as fout:
-                json.dump(Y_old, fout, indent=4)
-
-            if self.verbose >= 1:
-                print()
-
-        self.rankj = rankj
-
-        return Y_old
-
-    def predict(self, X):
-        return self.rankj.predict(X)
+    def transform(self, x, *args, **kwargs):
+        return super().transform(x[:, -1].tolist(), *args, **kwargs)
 
 
-def load_dataset(path):
-    with open(path, 'rb') as fin:
-        data = pickle.load(fin)
-    return data
+class MyTfidfVectorizer(TfidfVectorizer):
+    """
+    Extract the last column of the input matrix (which is app description) and
+    simply pass it to TfidfVectorizer.
+
+    Parameters
+    ----------
+    (identical to the parameters of TfidfVectorizer)
+    """
+
+    def __init__(self, input='content', encoding='utf-8', decode_error='strict',
+                 strip_accents=None, lowercase=True, preprocessor=None, tokenizer=None,
+                 analyzer='word', stop_words=None, token_pattern=r'(?u)\b\w\w+\b',
+                 ngram_range=(1, 1), max_df=1.0, min_df=1, max_features=None,
+                 vocabulary=None, binary=False, dtype=np.int64, norm='l2', use_idf=True,
+                 smooth_idf=True, sublinear_tf=False):
+
+        return super().__init__(input=input, encoding=encoding, decode_error=decode_error,
+                                strip_accents=strip_accents, lowercase=lowercase, preprocessor=preprocessor,
+                                tokenizer=tokenizer, analyzer=analyzer, stop_words=stop_words,
+                                token_pattern=token_pattern, ngram_range=ngram_range, max_df=max_df,
+                                min_df=min_df, max_features=max_features, vocabulary=vocabulary,
+                                binary=binary, dtype=dtype, norm=norm, use_idf=use_idf,
+                                smooth_idf=smooth_idf, sublinear_tf=sublinear_tf)
+
+    def fit(self, x, *args, **kwargs):
+        return super().fit(x[:, -1].tolist(), *args, **kwargs)
+
+    def fit_transform(self, x, *args, **kwargs):
+        return super().fit_transform(x[:, -1].tolist(), *args, **kwargs)
+
+    def transform(self, x, *args, **kwargs):
+        return super().transform(x[:, -1].tolist(), *args, **kwargs)
+
+
+class MyScaler(preprocessing.StandardScaler):
+    """
+    Standardize features by removing the mean and scaling to unit variance.
+
+    Parameters
+    ----------
+    (null)
+
+    Attributes
+    ----------
+    mean_: numpy.array, shape (n_features)
+        the mean value for each features in the input matrix
+    std_: numpy.array, shape (n_features)
+        the standard deviation for each features in the input matrix
+    """
+
+    def __init__(self):
+        self.mean_ = None
+        self.std_ = None
+        self.epsilon = 0.0001
+
+    def fit(self, X, y=None):
+        X = X[:, :-1].astype('float64')
+        self.mean_ = np.mean(X, axis=0)
+        self.std_ = np.std(X, axis=0)
+        return self
+
+    def fit_tranform(self, X, y=None):
+        X = X[:, :-1].astype('float64')
+        self.mean_ = np.mean(X, axis=0)
+        self.std_ = np.std(X, axis=0)
+        return (X - self.mean_) / (self.std_ + self.epsilon)
+
+    def transform(self, X, y=None):
+        X = X[:, :-1].astype('float64')
+        return (X - self.mean_) / (self.std_ + self.epsilon)
 
 
 def main(args):
     """
+    Grid-search over different parameters for a linear SVM classifier using
+    a 3-fold cross valiadation.
+
     args: argparse.Namespace object
 
     Returns: None
     """
-    x_train, y_train, x_test, y_test = load_dataset(args.infile)
 
-    # convert label to one-hot encoding
-    lb = preprocessing.LabelBinarizer()
-    y_train = lb.fit_transform(y_train.astype('int64')).tolist()
-    y_test = lb.transform(y_test.astype('int64')).tolist()
+    # load dataset
+    with open(path, 'rb') as fin:
+        x_train, y_train, x_test, y_test = lpickle.load(fin)
 
-    # extract tf-idf features
-    tfidf = TfidfVectorizer()
-    x_train = tfidf.fit_transform(x_train[:, -1])
-    x_test = tfidf.transform(x_test[:, -1])
+    y_train = y_train.astype('int64')
+    y_test = y_test.astype('int64')
 
-    clf = MultiRankClassifier(svm.LinearSVC(C=0.3), verbose=4)
+    # combined different features
+    feature_extractors = [
+        # ('general', MyScaler()),
+        # ('wordcount', MyCountVectorizer(ngram_range=(1, 1))),
+        ('tfidf', MyTfidfVectorizer()),
+    ]
+    combined_feature = FeatureUnion(feature_extractors)
 
-    train_rank = clf.fit(x_train, y_train)
-    test_rank = clf.predict(x_test)
+    estimators = [('feature', combined_feature),
+                  ('clf', svm.LinearSVC())]
+    pipeline = Pipeline(estimators)
 
-    print(len(train_rank), len(y_train))
-    print(len(test_rank), len(y_test))
+    # parameters to search
+    param_grid = [
+        {
+            'clf': [svm.LinearSVC()],
+            'clf__C': [1, 0.5, 0.3, 0.1, 0.03],
+        },
+    ]
 
-    pprint(train_rank[:10])
-    pprint(y_train[:10])
+    # start training
+    t0 = time.time()
+    grid = GridSearchCV(pipeline, param_grid=param_grid, verbose=4)
+    grid.fit(x_train, y_train)
 
-    pprint(test_rank[:10])
-    pprint(y_test[:10])
-
-    with open('model/rank', 'wb') as fout:
-        pickle.dump([train_rank, test_rank], fout)
+    print()
+    print('done in %.2f seconds' % (time.time() - t0))
+    print()
+    print('train accuracy: %.2f%%' % (100 * grid.score(x_train, y_train)))
+    print('test accuracy: %.2f%%' % (100 * grid.score(x_test, y_test)))
+    print()
+    print('the best parameters are:', grid.best_params_)
+    print()
+    print('confusion matrix:')
+    print(metrics.confusion_matrix(y_test, grid.predict(x_test)))
 
 
 if __name__ == '__main__':
